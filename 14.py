3b8eff0835410c121b4c26e10e6f32b518a8f85d
miabozzetto <mbozzeto@escuelasproa.edu.ar>
2026-05-19 12:46:38 -0300

    Add script to read and sort student math scores

---
+++ 14.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df['math score'])
+
+print(df.sort_values('math score'))
+
+print(df.sort_values('math score', ascending=False))
